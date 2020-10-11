a9e57dfb70455ab7c7e046a8bab0c9f52b965d44
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2020-10-11 04:02:10 -0700

    Update github_oath_credentials.dart

--- desktop/lib/github_oath_credentials.dart
+++ desktop/lib/github_oath_credentials.dart
@@ -1,4 +1,4 @@
-const githubClientId = '';
-const githubClientSecret = '';
+const githubClientId = '37e72c998e94387159d4';
+const githubClientSecret =  'a932863879c1a043d06795e3988a0074e997a5ac';
 
 const githubScopes = ['repo', 'read:org'];
